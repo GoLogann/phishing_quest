b69b83b4bb6200b70d8c1eaaf9ac22deef34eeab
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-06-02 10:44:59 -0300

    feat: criando background para desenvolvimento

---
+++ lib/app/data/constants/environments.dart
@@ -0,0 +1,19 @@
+import 'package:phishing_quest/app/data/models/constants/environment_properties.model.dart';
+
+/// Representing local environment configuration
+const EnvironmentProperties LOCAL_ENV = EnvironmentProperties(
+  cacheLifespan: 60000, // 1 minute
+  endpoint: EndpointProperties(
+    projeto: 'https://ba0e26373dc0.ngrok.app/api/v1', //8082
+    timeout: 1000,
+  ),
+);
+
+/// Representing production environment configuration
+const EnvironmentProperties PRODUCTION_ENV = EnvironmentProperties(
+  cacheLifespan: 300000, // 5 minutes
+  endpoint: EndpointProperties(
+    projeto: 'http://projeto-plataformagt.gtconsultoria.com.br/api/v1',
+    timeout: 80,
+  ),
+);
