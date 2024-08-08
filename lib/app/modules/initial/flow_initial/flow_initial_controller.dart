6575fb0ef7ea75efa970e69302d15d39534a192a
Logan Cardoso <logan.cc@outlook.com>
2024-08-08 14:47:20 -0300

    wip: alterando flow initial controller para chamar tela de criação de usuário.

--- lib/app/modules/initial/flow_initial/flow_initial_controller.dart
+++ lib/app/modules/initial/flow_initial/flow_initial_controller.dart
@@ -1,5 +1,10 @@
-import '../../../data/controllers/base_controller.dart';
+import 'package:get/get.dart';
+import 'package:phishing_quest/app/data/controllers/base_controller.dart';
+import 'package:phishing_quest/app/modules/register/userRegister/user_register_module.dart';
 
 class FlowInitialController extends BaseController {
 
+  void onRegister() {
+    Get.toNamed(UserRegisterModule.path);
+  }
 }
