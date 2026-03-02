30b8bcf204eb63c8a57895b84312f6a77cab294b
Logan Cardoso <davison.cardoso@dnxbrasil.com>
2026-03-02 13:55:09 -0300

    feat: adicionar tema padrão e ajustar estilo da barra de status no aplicativo

--- lib/app/main_getx_app.dart
+++ lib/app/main_getx_app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
+import 'package:phishing_quest/app/global_ui/app_theme.dart';
 import 'package:phishing_quest/app/routes/app_pages.dart';
 
 class MainGetXApp extends StatelessWidget {
@@ -13,14 +14,19 @@ class MainGetXApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
     SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(
-        statusBarBrightness: Brightness.light,
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.light,
       ),
     );
 
     return GetMaterialApp(
       title: 'Phishing Quest',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.defaultTheme,
       initialRoute: initialPage,
       getPages: AppPages.routes,
       locale: const Locale('pt', 'BR'),
